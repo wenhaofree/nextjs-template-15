fea1b1d02f3ec3ea85bb8d47dc6b75b4b3fa190d
fwh-mac-mini <fuwenhao594@163.com>
2024-11-18 15:02:36 +0800

    add: 谷歌登录测试版

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { compare } from 'bcryptjs'
 import { Pool } from 'pg'
@@ -11,6 +12,17 @@ const pool = new Pool({
 
 const handler = NextAuth({
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    }),
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
