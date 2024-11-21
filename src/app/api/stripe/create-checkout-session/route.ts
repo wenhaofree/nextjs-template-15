ae9bc059b9a6fb52339f611bdd8cf544c381ebbe
fwh-mac-mini <fuwenhao594@163.com>
2024-11-21 17:23:05 +0800

    fix: stripe version

--- src/app/api/stripe/create-checkout-session/route.ts
+++ src/app/api/stripe/create-checkout-session/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_TEST_SECRET_KEY!, {
-  apiVersion: process.env.STRIPE_API_VERSION as '2024-10-28.acacia',
+  apiVersion: process.env.STRIPE_API_VERSION as Stripe.LatestApiVersion,
 })
 
 const PRICE_IDS = {
