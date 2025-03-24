3bc596bbb670c1744fce5c93be057e0ae1ccb729
FwhMacMini <fuwenhao594@163.com>
2025-03-24 17:12:34 +0800

    注释首页

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -33,7 +33,7 @@ export default async function LandingPage({
   return (
     <>
       {page.hero && <Hero hero={page.hero} />}
-      {page.branding && <Branding section={page.branding} />}
+      {/* {page.branding && <Branding section={page.branding} />}
       {page.introduce && <Feature1 section={page.introduce} />}
       {page.benefit && <Feature2 section={page.benefit} />}
       {page.usage && <Feature3 section={page.usage} />}
@@ -43,7 +43,7 @@ export default async function LandingPage({
       {page.pricing && <Pricing pricing={page.pricing} />}
       {page.testimonial && <Testimonial section={page.testimonial} />}
       {page.faq && <FAQ section={page.faq} />}
-      {page.cta && <CTA section={page.cta} />}
+      {page.cta && <CTA section={page.cta} />} */}
     </>
   );
 }
