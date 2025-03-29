4d44dacb793482f1af9f0f04f2d46eae1f74f166
FwhMacMini <fuwenhao594@163.com>
2025-03-29 10:01:32 +0800

    feat： 主题切换

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,20 @@
 'use client';
 
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <ThemeProvider 
+        attribute="class" 
+        defaultTheme="system" 
+        enableSystem
+        enableColorScheme
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
