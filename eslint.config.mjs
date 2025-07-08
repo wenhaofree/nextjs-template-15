d824e98dc5b2619103e7316eb75c0c42c6b8a285
wenhao-macmini <fuwenhao594@163.com>
2025-07-08 10:10:47 +0800

    feat: 升级客户端

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ['next/core-web-vitals'],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   }),
 ]
